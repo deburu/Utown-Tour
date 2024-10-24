b66bbe5d9048a93da1c6edda3165b552bd6458de
deburu <e0053316@u.nus.edu>
2024-10-24 15:52:33 +0800

    First commit

---
+++ data.js
@@ -0,0 +1,206 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-erc_level-1_nearescalator_photo_1512",
+      "name": "ERC_Level 1_NearEscalator_PHOTO_1512",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 3.0848636912659497,
+        "pitch": 0.04211187801845995,
+        "fov": 1.1458037731858022
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.388146216875441,
+          "pitch": 0.10094967344848982,
+          "rotation": 0,
+          "target": "2-erc_level-1_tembusu-tree"
+        },
+        {
+          "yaw": -1.000574129288129,
+          "pitch": -0.00040295857518302114,
+          "rotation": 10.210176124166829,
+          "target": "1-erc_level-1_starbucks"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.069900828736869,
+          "pitch": 0.144948327835273,
+          "title": "ERC Drop-off/Pick-up Point",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.500408246880566,
+          "pitch": 0.07269910139428148,
+          "title": "Connector to Stephen Riady Centre (SRC)",
+          "text": "Many food options there! Highly recommend the yong tau foo store!"
+        },
+        {
+          "yaw": -0.46155189197246216,
+          "pitch": 0.012927362029980571,
+          "title": "Green space&nbsp;",
+          "text": "Open green space for activities and a common photo taking spot!"
+        },
+        {
+          "yaw": -0.9934801140257576,
+          "pitch": 0.11664712377166353,
+          "title": "Route down to Starbucks",
+          "text": "<br>"
+        },
+        {
+          "yaw": -1.5593792253752632,
+          "pitch": 0.08397536083763413,
+          "title": "2nd Floor of ERC",
+          "text": "Has more sectional teaching rooms where tutorials are held. Has many tables and seats for self-study both in air-conditioned and non-air-conditioned environment."
+        }
+      ]
+    },
+    {
+      "id": "1-erc_level-1_starbucks",
+      "name": "ERC_Level 1_Starbucks",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4444415672830928,
+          "pitch": 0.034596669776695066,
+          "rotation": 5.497787143782138,
+          "target": "0-erc_level-1_nearescalator_photo_1512"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4846997120839376,
+          "pitch": 0.17009038842454238,
+          "title": "Vending machine food",
+          "text": "Text"
+        },
+        {
+          "yaw": -3.0232543949622457,
+          "pitch": 0.06371194410245096,
+          "title": "Starbucks entrance",
+          "text": "<br>"
+        },
+        {
+          "yaw": 0.15735174746079394,
+          "pitch": 0.10330431476557678,
+          "title": "Sheltered space with tables for use",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.7134489978069674,
+          "pitch": -0.03923722989543599,
+          "title": "Town Plaza",
+          "text": "Where events, booths are usually held. also has many nice food options! Try Hwangs!"
+        }
+      ]
+    },
+    {
+      "id": "2-erc_level-1_tembusu-tree",
+      "name": "ERC_Level 1_Tembusu Tree",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5370273421172325,
+          "pitch": 0.13187839637444299,
+          "rotation": 0,
+          "target": "0-erc_level-1_nearescalator_photo_1512"
+        },
+        {
+          "yaw": 2.5250676061821,
+          "pitch": 0.1099126137896782,
+          "rotation": 3.141592653589793,
+          "target": "1-erc_level-1_starbucks"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7860616922221926,
+          "pitch": 0.5426003239586699,
+          "title": "Seats for chilling :)",
+          "text": "Good photo spot too!"
+        },
+        {
+          "yaw": 2.877526952816302,
+          "pitch": -0.43424073572975175,
+          "title": "3rd Floor of ERC",
+          "text": "Open space area for activities or just chilling."
+        }
+      ]
+    }
+  ],
+  "name": "UTown in my words",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
